0b4eb628c1ae9b3776b95411e9c700d582639d6f
ShaHz <shahz@ShaHzs-iMac.local>
2019-02-20 14:46:45 +0000

    Database updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_004451) do
+ActiveRecord::Schema.define(version: 2019_02_20_144530) do
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2019_02_13_004451) do
     t.integer "pageid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "page_image_file_name"
+    t.string "page_image_content_type"
+    t.bigint "page_image_file_size"
+    t.datetime "page_image_updated_at"
   end
 
 end
